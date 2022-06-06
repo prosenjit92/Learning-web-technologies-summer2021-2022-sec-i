7cba8bcb151656383fd74deed12be6da7843c837
prosenjit92 <101933768+prosenjit92@users.noreply.github.com>
2022-06-06 13:46:56 +0600

    PHP

---
+++ 1_rectangle_perimeter.php
@@ -0,0 +1,10 @@
+<?php
+	$length = 10;
+	$width = 5;
+	$rectangle = $length * $width;
+	$pepimeter = 2 * ($length * $width);
+	echo "Rectangle = " .$rectangle."<br>";
+	echo "Perimeter = " .$pepimeter;
+	
+	
+?>
---
+++ 6_search_element.php
@@ -0,0 +1,19 @@
+<?php
+
+	$num = array(1,2,5,7,9,15,20,40);
+
+	if(in_array("9",$num))
+	{
+
+		echo "found";
+
+	}
+
+	else
+	{
+
+		echo "not found";
+
+	}
+
+?>
