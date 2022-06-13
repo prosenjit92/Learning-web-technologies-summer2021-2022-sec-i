8507f3eb7ec9a657df3638b5eb07be6b8ffcdc22
prosenjit biswas <pb117702@gmail.com>
2022-06-13 17:00:39 +0600

    Added

---
+++ Lab 2 php/2_vat.php
@@ -0,0 +1,5 @@
+<?php
+	$amount = 75;
+	$vat =15*$amount/100;
+	echo "Vat = " .$vat;
+?> 
---
+++ Lab 2 php/3_odd_even.php
@@ -0,0 +1,16 @@
+<?php
+	$number = 50;
+	$ans = $number%2;
+
+	if($ans == 1)
+	{
+		echo $number ;
+		echo "is odd Number ";
+	}
+	else
+	{
+		echo $number ;
+		echo " is even Number ";
+	}
+
+?> 
